2f2c801f3edbfa2de569061671daa59a91820f81
NicolaKuehnert <nicola.kuehnert@outlook.de>
2020-05-06 11:50:26 +0200

    Besseres Refactoring

--- main.cpp
+++ main.cpp
@@ -11,11 +11,12 @@
 
 #include "GLSLProgram.h"
 #include "GLTools.h"
+#include "Object.h"
 
 // Standard window width
-const int WINDOW_WIDTH  = 640;
+const int WINDOW_WIDTH  = 1200;
 // Standard window height
-const int WINDOW_HEIGHT = 480;
+const int WINDOW_HEIGHT = 1000;
 // GLUT window id/handle
 int glutID = 0;
 
@@ -30,156 +31,55 @@ float zFar  = 100.0f;
 /*
 Struct to hold data for object rendering.
 */
-class Object
-{
-public:
-  inline Object ()
-    : vao(0),
-      positionBuffer(0),
-      colorBuffer(0),
-      indexBuffer(0)
-  {}
 
-  inline ~Object () { // GL context must exist on destruction
-    glDeleteVertexArrays(1, &vao);
-    glDeleteBuffers(1, &indexBuffer);
-    glDeleteBuffers(1, &colorBuffer);
-    glDeleteBuffers(1, &positionBuffer);
-  }
-
-  GLuint vao;        // vertex-array-object ID
-  
-  GLuint positionBuffer; // ID of vertex-buffer: position
-  GLuint colorBuffer;    // ID of vertex-buffer: color
-  
-  GLuint indexBuffer;    // ID of index-buffer
-  
-  glm::mat4x4 model; // model matrix
-};
 
 Object triangle;
 Object quad;
+Object quad2;
 
 void renderTriangle()
 {
-  // Create mvp.
-  glm::mat4x4 mvp = projection * view * triangle.model;
-  
-  // Bind the shader program and set uniform(s).
-  program.use();
-  program.setUniform("mvp", mvp);
-  
-  // Bind vertex array object so we can render the 1 triangle.
-  glBindVertexArray(triangle.vao);
-  glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_SHORT, 0);
-  glBindVertexArray(0);
+    triangle.render(GL_TRIANGLES, 3, view, projection, program);
+   
 }
 
 void renderQuad()
 {
-  // Create mvp.
-  glm::mat4x4 mvp = projection * view * quad.model;
-  
-  // Bind the shader program and set uniform(s).
-  program.use();
-  program.setUniform("mvp", mvp);
-  
-  // Bind vertex array object so we can render the 2 triangles.
-  glBindVertexArray(quad.vao);
-  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
-  glBindVertexArray(0);
+    quad.render(GL_TRIANGLES, 6, view, projection, program);
+    quad2.render(GL_TRIANGLES, 6, view, projection, program);
+    
 }
 
 void initTriangle()
 {
-  // Construct triangle. These vectors can go out of scope after we have send all data to the graphics card.
-  const std::vector<glm::vec3> vertices = { glm::vec3(-1.0f, 1.0f, 0.0f), glm::vec3(1.0f, -1.0f, 0.0f), glm::vec3(1.0f, 1.0f, 0.0f) };
-  const std::vector<glm::vec3> colors   = { glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f) };
-  const std::vector<GLushort>  indices  = { 0, 1, 2 };
+    // Kanten, Farben, Indizes zuweisen
+    triangle.vertices = { glm::vec3(-1.0f, 1.0f, 0.0f), glm::vec3(1.0f, -1.0f, 0.0f), glm::vec3(1.0f, 1.0f, 0.0f) };
+    triangle.colors = { glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f) };
+    triangle.indices = { 0, 1, 2 };
 
-  GLuint programId = program.getHandle();
-  GLuint pos;
+    GLuint programId = program.getHandle();
 
-  // Step 0: Create vertex array object.
-  glGenVertexArrays(1, &triangle.vao);
-  glBindVertexArray(triangle.vao);
-  
-  // Step 1: Create vertex buffer object for position attribute and bind it to the associated "shader attribute".
-  glGenBuffers(1, &triangle.positionBuffer);
-  glBindBuffer(GL_ARRAY_BUFFER, triangle.positionBuffer);
-  glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), vertices.data(), GL_STATIC_DRAW);
-  
-  // Bind it to position.
-  pos = glGetAttribLocation(programId, "position");
-  glEnableVertexAttribArray(pos);
-  glVertexAttribPointer(pos, 3, GL_FLOAT, GL_FALSE, 0, 0);
-  
-  // Step 2: Create vertex buffer object for color attribute and bind it to...
-  glGenBuffers(1, &triangle.colorBuffer);
-  glBindBuffer(GL_ARRAY_BUFFER, triangle.colorBuffer);
-  glBufferData(GL_ARRAY_BUFFER, colors.size() * sizeof(glm::vec3), colors.data(), GL_STATIC_DRAW);
-  
-  // Bind it to color.
-  pos = glGetAttribLocation(programId, "color");
-  glEnableVertexAttribArray(pos);
-  glVertexAttribPointer(pos, 3, GL_FLOAT, GL_FALSE, 0, 0);
-  
-  // Step 3: Create vertex buffer object for indices. No binding needed here.
-  glGenBuffers(1, &triangle.indexBuffer);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, triangle.indexBuffer);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLushort), indices.data(), GL_STATIC_DRAW);
-  
-  // Unbind vertex array object (back to default).
-  glBindVertexArray(0);
-  
-  // Modify model matrix.
-  triangle.model = glm::translate(glm::mat4(1.0f), glm::vec3(-1.25f, 0.0f, 0.0f));
+    // Dreiecke initialisieren
+    triangle.init(programId, glm::vec3(-1.25f, 0.0f, 0.0f));
+    
 }
 
 void initQuad()
 {
-  // Construct triangle. These vectors can go out of scope after we have send all data to the graphics card.
-  const std::vector<glm::vec3> vertices = { { -1.0f, 1.0f, 0.0f }, { -1.0, -1.0, 0.0 }, { 1.0f, -1.0f, 0.0f }, { 1.0f, 1.0f, 0.0f } };
-  const std::vector<glm::vec3> colors   = { { 1.0f, 0.0f, 0.0f }, { 0.0f, 1.0, 1.0f }, { 0.0f, 1.0f, 0.0f }, { 0.0f, 0.0f, 1.0f } };
-  const std::vector<GLushort>  indices  = { 0, 1, 2, 0, 2, 3 };
+    // Kanten, Farben, Indizes zuweisen
+    quad.vertices = { { -1.0f, 1.0f, 0.0f }, { -1.0, -1.0, 0.0 }, { 1.0f, -1.0f, 0.0f }, { 1.0f, 1.0f, 0.0f } };
+    quad.colors = { { 1.0f, 0.0f, 0.0f }, { 0.0f, 1.0, 1.0f }, { 0.0f, 1.0f, 0.0f }, { 0.0f, 0.0f, 1.0f } };
+    quad.indices = { 0, 1, 2, 0, 2, 3 };
 
-  GLuint programId = program.getHandle();
-  GLuint pos;
-  
-  // Step 0: Create vertex array object.
-  glGenVertexArrays(1, &quad.vao);
-  glBindVertexArray(quad.vao);
-  
-  // Step 1: Create vertex buffer object for position attribute and bind it to the associated "shader attribute".
-  glGenBuffers(1, &quad.positionBuffer);
-  glBindBuffer(GL_ARRAY_BUFFER, quad.positionBuffer);
-  glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), vertices.data(), GL_STATIC_DRAW);
-  
-  // Bind it to position.
-  pos = glGetAttribLocation(programId, "position");
-  glEnableVertexAttribArray(pos);
-  glVertexAttribPointer(pos, 3, GL_FLOAT, GL_FALSE, 0, 0);
-  
-  // Step 2: Create vertex buffer object for color attribute and bind it to...
-  glGenBuffers(1, &quad.colorBuffer);
-  glBindBuffer(GL_ARRAY_BUFFER, quad.colorBuffer);
-  glBufferData(GL_ARRAY_BUFFER, colors.size() * sizeof(glm::vec3), colors.data(), GL_STATIC_DRAW);
-  
-  // Bind it to color.
-  pos = glGetAttribLocation(programId, "color");
-  glEnableVertexAttribArray(pos);
-  glVertexAttribPointer(pos, 3, GL_FLOAT, GL_FALSE, 0, 0);
-  
-  // Step 3: Create vertex buffer object for indices. No binding needed here.
-  glGenBuffers(1, &quad.indexBuffer);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, quad.indexBuffer);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLushort), indices.data(), GL_STATIC_DRAW);
-  
-  // Unbind vertex array object (back to default).
-  glBindVertexArray(0);
-  
-  // Modify model matrix.
-  quad.model = glm::translate(glm::mat4(1.0f), glm::vec3(1.25f, 0.0f, 0.0f));
+    quad2.vertices = { { -1.0f, 1.0f, 0.0f }, { -1.0, -1.0, 0.0 }, { 1.0f, -1.0f, 0.0f }, { 1.0f, 1.0f, 0.0f } };
+    quad2.colors = { { 1.0f, 0.0f, 0.0f }, { 0.0f, 1.0, 1.0f }, { 0.0f, 1.0f, 0.0f }, { 0.0f, 0.0f, 1.0f } };
+    quad2.indices = { 0, 1, 2, 0, 2, 3 };
+
+    GLuint programId = program.getHandle();
+
+    // Quadrate initialisieren
+    quad.init(programId, glm::vec3(1.25f, 1.0f, 0.0f));
+    quad2.init(programId, glm::vec3(1.25f, -1.10f, 0.0f));
 }
 
 /*
@@ -192,6 +92,7 @@ bool init()
   glEnable(GL_DEPTH_TEST);
   
   // Construct view matrix.
+  // Camera
   glm::vec3 eye(0.0f, 0.0f, 4.0f);
   glm::vec3 center(0.0f, 0.0f, 0.0f);
   glm::vec3 up(0.0f, 1.0f, 0.0f);
@@ -283,6 +184,7 @@ void glutKeyboard (unsigned char keycode, int x, int y)
 int main(int argc, char** argv)
 {
   // GLUT: Initialize freeglut library (window toolkit).
+  // Init Window
   glutInitWindowSize    (WINDOW_WIDTH, WINDOW_HEIGHT);
   glutInitWindowPosition(40,40);
   glutInit(&argc, argv);
@@ -292,6 +194,7 @@ int main(int argc, char** argv)
   glutInitContextFlags  (GLUT_FORWARD_COMPATIBLE | GLUT_DEBUG);
   glutInitDisplayMode   (GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH | GLUT_MULTISAMPLE);
   
+  //Set window name
   glutCreateWindow("Aufgabenblatt 01");
   glutID = glutGetWindow();
   
